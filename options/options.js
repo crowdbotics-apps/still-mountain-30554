cdae1ad8cf32b3ad6012e8595dd10cfe4f4cb172
Crowdbotics <team@crowdbotics.com>
2021-09-15 02:17:38 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "still_mountain_30554Identifier",
+  url: "https://still_mountain_30554Identifier.botics.co",
+  api: "https://still_mountain_30554Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
